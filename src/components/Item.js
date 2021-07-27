43fea2c546c350110ea7d95787c27c5fd67c32ec
Emilio E. Hurtado <emiliohurtado1@gmail.com>
2021-07-27 19:22:30 -0300

    configuré react-elastic-carousel

---
+++ src/components/Item.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export default styled.div`
+    display:flex;
+    justify-content: center;
+    align-items: center;
+    height: 550px;
+    width: 100%;
+    margin: 0px 15px;
+`;
--- src/components/ListaPeliculas.js
+++ src/components/ListaPeliculas.js
@@ -1,25 +1,39 @@
-import React from "react";
+import React, { Component } from "react";
 import { Card } from "react-bootstrap";
+import Carousel from "react-elastic-carousel";
+import Item from "./Item";
+
+const breakPoints = [
+  { width: 1, itemsToShow: 1 },
+  { width: 550, itemsToShow: 2 },
+  { width: 768, itemsToShow: 4 },
+  { width: 1200, itemsToShow: 4 },
+];
 
 const ListaPeliculas = (props) => {
   const ComponentFavoritas = props.componentFavoritas;
 
   return (
     <div>
-      {props.peliculas.map((pelicula, index) => (
-        <div className="mb-4 col-md-3 col-sm-12">
-          <Card className="cardContenedor">
-            <Card.Img variant="top" src={pelicula.Poster} />
-            <Card.Body>
-              <Card.Title>{pelicula.Title}</Card.Title>
-            </Card.Body>
-            <Card.Footer>{pelicula.Year}</Card.Footer>
-            <div onClick={() => props.handleFavoritasClick(pelicula)} className="overlay">
-              <ComponentFavoritas></ComponentFavoritas>
-            </div>
-          </Card>
-        </div>
-      ))}
+      <Carousel breakPoints={breakPoints}>
+        {props.peliculas.map((pelicula, index) => (
+          <Item>
+            <Card className="cardContenedor">
+              <Card.Img variant="top" src={pelicula.Poster} />
+              <Card.Body>
+                <Card.Title>{pelicula.Title}</Card.Title>
+              </Card.Body>
+              <Card.Footer>{pelicula.Year}</Card.Footer>
+              <div
+                onClick={() => props.handleFavoritasClick(pelicula)}
+                className="overlay"
+              >
+                <ComponentFavoritas></ComponentFavoritas>
+              </div>
+            </Card>
+          </Item>
+        ))}
+      </Carousel>
     </div>
   );
 };
