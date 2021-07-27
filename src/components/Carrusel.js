1b72f0c481bc20fcc45edcb4c318128f0c9b3495
Emilio E. Hurtado <emiliohurtado1@gmail.com>
2021-07-27 18:16:37 -0300

    corregí error de falta de condicional en useEffect al cargar la página sin LS

--- src/components/Carrusel.js
+++ src/components/Carrusel.js
@@ -10,21 +10,36 @@ const Carrusel = (props) => {
   return (
     <div className="mb-4">
       <h3 className="text-light">{props.titulo}</h3>
-      <OwlCarousel items={5} className="owl-theme" nav margin={8}>
+      <OwlCarousel
+        items={5}
+        className="owl-theme"
+        dots
+        autoplay
+        autoplayHoverPause
+        callbacks
+        info
+        margin={8}
+      >
         {props.peliculas.map((pelicula, index) => (
-          <Card className="cardContenedor">
-            <Card.Img variant="top" src={pelicula.Poster} alt="poster película" />
-            <Card.Body>
-              <Card.Title>{pelicula.Title}</Card.Title>
-            </Card.Body>
-            <Card.Footer>{pelicula.Year}</Card.Footer>
-            <div
-              onClick={() => props.handleFavoritasClick(pelicula)}
-              className="overlay"
-            >
-              <ComponentFavoritas></ComponentFavoritas>
-            </div>
-          </Card>
+          <div itemElement className="">
+            <Card className="cardContenedor">
+              <Card.Img
+                variant="top"
+                src={pelicula.Poster}
+                alt="poster película"
+              />
+              <Card.Body>
+                <Card.Title>{pelicula.Title}</Card.Title>
+              </Card.Body>
+              <Card.Footer>{pelicula.Year}</Card.Footer>
+              <div
+                onClick={() => props.handleFavoritasClick(pelicula)}
+                className="overlay"
+              >
+                <ComponentFavoritas></ComponentFavoritas>
+              </div>
+            </Card>
+          </div>
         ))}
       </OwlCarousel>
     </div>
